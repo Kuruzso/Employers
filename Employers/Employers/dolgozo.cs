4663e7689257d1a28bc692fad44f8f8fb02c9f48
Veller_Arpad <vellera@kkszki.hu>
2024-09-17 11:53:44 +0200

    1.feladat

---
+++ Employers/Employers/dolgozo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employers
+{
+    internal class dolgozo
+    {
+
+        public int azonosito;
+        public string név;
+        public int kor;
+        public int kereset;
+
+        public dolgozo() {
+        
+        
+        
+        }
+    }
+}
