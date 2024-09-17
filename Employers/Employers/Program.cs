e6b0497a6778b1c2e0b9e4b8e770a61c48d9a79b
Veller_Arpad <vellera@kkszki.hu>
2024-09-17 12:30:49 +0200

    4. Írja ki azoknak a dolgozóknak az azonosítóját és nevét aki(k) a legjobban keresnek,

--- Employers/Employers/Program.cs
+++ Employers/Employers/Program.cs
@@ -12,16 +12,33 @@ namespace Employers
     {
         static void Main(string[] args)
         {
+            //1.feladat
             string[] adat = File.ReadAllLines("tulajdonsagok_100sor.txt");
+            //2.feladat
             List<dolgozo> tulajdonsagLista = new List<dolgozo>();
+
             foreach (var sor in adat)
             {
                 tulajdonsagLista.Add(new dolgozo(sor));
             }
+            //3.feladat
             foreach (var item in tulajdonsagLista)
             {
                 Console.WriteLine(item.nev);
             }
+            //4.feladat
+            int index = 0;
+            for (int i = 0; i < tulajdonsagLista.Count; i++)
+            {
+                if (tulajdonsagLista[index].kereset < tulajdonsagLista[i].kereset)
+                {
+                    index = i;
+                }
+            }
+            Console.WriteLine(tulajdonsagLista[index].azonosito+" ," + tulajdonsagLista[index].nev);
+
+            Console.ReadLine();
+
         }
     }
 }
