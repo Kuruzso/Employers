988d922910d284841edab7e648056e4dc85e55a5
Veller_Arpad <vellera@kkszki.hu>
2024-09-17 12:57:32 +0200

    feladat leírások

--- Employers/Employers/Program.cs
+++ Employers/Employers/Program.cs
@@ -13,20 +13,27 @@ namespace Employers
         static void Main(string[] args)
         {
             //1.feladat
+            Console.WriteLine("1.feladat : beolvassa a fájlt");
             string[] adat = File.ReadAllLines("tulajdonsagok_100sor.txt");
+            Console.WriteLine();
             //2.feladat
+            Console.WriteLine("2.feladat : eltárolja a fájlt");
             List<dolgozo> tulajdonsagLista = new List<dolgozo>();
 
             foreach (var sor in adat)
             {
                 tulajdonsagLista.Add(new dolgozo(sor));
             }
+            Console.WriteLine();
             //3.feladat
+            Console.WriteLine("3.feladat : megjeleníti az összes dolgozó nevét");
             foreach (var item in tulajdonsagLista)
             {
                 Console.WriteLine(item.nev);
             }
+            Console.WriteLine();
             //4.feladat
+            Console.WriteLine("4.feladat : ki írja  azon dolgozók azonosítóját és nevét akik a legjobban keresnek");
             int index = 0;
             for (int i = 0; i < tulajdonsagLista.Count; i++)
             {
@@ -36,7 +43,9 @@ namespace Employers
                 }
             }
             Console.WriteLine(tulajdonsagLista[index].azonosito+" ," + tulajdonsagLista[index].nev);
-
+            Console.WriteLine();
+            //5.feladat
+            Console.WriteLine("5.feladat : ki írja azoknak a nevét akiknek 10 évük van nyugdíjig");
             for (int i = 0; i < tulajdonsagLista.Count; i++)
             {
                 if (tulajdonsagLista[i].kor == 55)
@@ -44,6 +53,9 @@ namespace Employers
                     Console.WriteLine(tulajdonsagLista[i].nev+" " + tulajdonsagLista[i].kor);
                 }
             }
+            Console.WriteLine();
+            //6.feladat
+            Console.WriteLine("6.feladat : megszámolja hanyan keresnek 50000 felett");
             int hanyan = 0;
             for (int i = 0; i < tulajdonsagLista.Count; i++)
             {
